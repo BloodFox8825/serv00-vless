d29c94c6a7b489555aa05141d16541653d6ac228
BloodFox8825 <127063466+BloodFox8825@users.noreply.github.com>
2024-06-28 09:27:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,40 +5,46 @@ const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 
 // 定义常量
-const WS_PATH = 'YourPath';
+const VLESS_PATH = '/vless';
+const ECHO_PATH = '/echo'
 
 const uuid = 'YourGuid'.replace(/-/g, '');
 const port = YourPort;
 
 function onSocketError(err) {
-  console.error(err);
+    console.error(err);
 }
 
 // 创建HTTP服务器
 const server = http.createServer((req, res) => {
-  // 这里可以处理非WebSocket请求
-  console.error("bad request:", req.url);
-  res.writeHead(404, {'Content-Type': 'text/plain'});
-  res.end('Not Found\n');
+    // 这里可以处理非WebSocket请求
+    if (req.url === ECHO_PATH) {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(request.url);
+    } else {
+        console.error("bad request:", req.url);
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
 });
 
 // 检查upgrade请求的路径
 server.on('upgrade', (request, socket, head) => {
-  socket.on('error', onSocketError);
-  
-  // 使用常量WS_PATH来检查路径
-  if (request.url === WS_PATH) {
-    console.log('upgrade');
-    // 如果路径正确，继续WebSocket握手
-    wss.handleUpgrade(request, socket, head, (ws) => {
-	  console.log('handleUpgrade...');
-      wss.emit('connection', ws, request);
-    });
-  } else {
-	console.error('path wrong:', request.url);
-    // 否则，拒绝WebSocket升级请求
-    socket.destroy();
-  }
+    socket.on('error', onSocketError);
+
+    // 使用常量VLESS_PATH来检查路径
+    if (request.url === VLESS_PATH) {
+        console.log('upgrade');
+        // 如果路径正确，继续WebSocket握手
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            console.log('handleUpgrade...');
+            wss.emit('connection', ws, request);
+        });
+    } else {
+        console.error('path wrong:', request.url);
+        // 否则，拒绝WebSocket升级请求
+        socket.destroy();
+    }
 });
 
 // 创建WebSocket服务器，传入noServer选项
@@ -48,15 +54,15 @@ wss.on('connection', ws => {
     console.log("on connection");
 
     ws.once('message', msg => {
-	    console.log("on message");
+        console.log("on message");
         const [VERSION] = msg;
         const id = msg.slice(1, 17);
 
         if (!id.every((v, i) => v === parseInt(uuid.substr(i * 2, 2), 16))) {
-			console.error('Invalid client:', id.toString('hex'));
-			ws.terminate();
-			return;
-		}
+            console.error('Invalid client:', id.toString('hex'));
+            ws.terminate();
+            return;
+        }
 
         let i = msg.slice(17, 18).readUInt8() + 19;
         const targetPort = msg.slice(i, i += 2).readUInt16BE(0);
